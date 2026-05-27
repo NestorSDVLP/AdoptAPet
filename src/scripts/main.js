4387c181a3878cf613d85ce8f9203d9c9f467289
NestorSDVLP <nestors.dvlp@gmail.com>
2026-05-26 21:33:44 -0300

    Setup Vue/Vite project with Bootstrap, Pinia and Sass

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+import '@/assets/styles/main.scss'
+
+import App from '@/App.vue'
+import router from '@/router/index.js'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.use(router)
+
+app.mount('#app')
